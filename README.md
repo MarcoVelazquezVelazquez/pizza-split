0b26bcc330e603f5633fac40a0d5b626a8c5a249
Leonid Artemev <thesobrat@yandex.ru>
2021-07-02 10:42:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pizza Split
+
+Proceed to [PizzaSplit](src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java) class
+and write a program that reads number of people and number of pieces per pizza and then
+prints minimum number of pizzas to order to split all the pizzas equally and with no remainder.
+ 
---
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -0,0 +1,11 @@
+package com.epam.rd.autotasks.pizzasplit;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+        //Write a program, reading number of people and number of pieces per pizza and then
+        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+
+
+
+    }
+}
